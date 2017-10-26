657155bacbb8799748950af5e2b3b776f9e5f6a2
Alex Alajbegovic <aaalex@0587377532.wireless.umich.net>
2017-10-26 19:25:31 -0400

    Initial Commit

---
+++ 20AB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20AB
+//
+//  Created by Alex Alajbegovic on 10/26/17.
+//  Copyright © 2017 Alex Alajbegovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
